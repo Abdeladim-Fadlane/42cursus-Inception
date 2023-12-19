e095a2252ad31220fa580ceeb71edc2fe4f0f48f
Abdeladim-Fadlane <Abdofadlane128@gmail.com>
2023-12-19 15:45:14 +0100

    update

---
+++ srcs/requirements/tools/script copy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+service mariadb start
+sleep 5
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+
+mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+
+mysqld_safe
---
+++ srcs/requirements/tools/script.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+mkdir /run/php/
+# Wait for MariaDB to be ready
+while ! mariadb -h mariadb -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" ; do
+    echo "Establishing a database connection  ...";
+    sleep 3
+done
+
+wp config create	--allow-root \
+    --dbname=$MYSQL_DATABASE \
+    --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD \
+    --dbhost=mariadb:3306 \
+    --path='/var/www/wordpress'
+    
+# WordPress installation
+wp core install --allow-root \
+    --url="$DOMINE_NAME" \
+    --title="$WP_TITLE" \
+    --admin_user="$WP_ADMIN" \
+    --admin_password="$WP_ADMIN_PW" \
+    --admin_email="$WP_ADMIN_ML" \
+    --path="/var/www/wordpress"
+
+wp user create --allow-root "$WP_USER"\
+    "$WP_USER_ML"\
+    --role=editor\
+    --user_pass="$WP_USER_PW"\
+    --path="/var/www/wordpress"
+
+# Start PHP-FPM AND  run PHP-FPM in the foreground NOT DAEMON
+php-fpm7.4 -F
